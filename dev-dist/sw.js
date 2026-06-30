566df6b21159f74040903d021a0112b2ac0d19b0
AcepAlinurdin <acepali2253@gmail.com>
2026-06-30 23:58:42 +0700

    feat: add service worker implementation using Workbox for asset caching and offline support

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-d3f34149'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6fjuc1tm7eo"
+    "revision": "0.3mm48n2rj0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
